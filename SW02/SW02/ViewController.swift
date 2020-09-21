cc5a50ea8ef635f8ad55f8cf19c8e4512992f61a
Simon Meier <63072220+simimeier@users.noreply.github.com>
2020-09-21 19:14:01 +0200

    init

---
+++ SW02/SW02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SW02
+//
+//  Created by Simon Meier on 21.09.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
